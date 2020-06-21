26b1e07e8db4d29641f1c36b557fb04e453fd42f
Mochammad Fauzan Rizqulloh <mochammadfauzanrizqulloh@gmail.com>
2020-06-21 16:12:18 +0700

    update source
    adding necessary module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
+  imports: [
+    // config module
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+    }),
+    // cache module
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        ttl: configService.get('CACHE_TTL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,51 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // config
+  const configService = app.get(ConfigService);
+  const APP_PORT = configService.get('APP_PORT')
+  const RATE_LIMIT_MS = +configService.get('RATE_LIMIT_MS')
+  const RATE_LIMIT_MAX = +configService.get('RATE_LIMIT_MAX')
+
+  // compression
+  app.use(compression());
+
+  // helmet
+  app.use(helmet());
+
+  // rate limit
+  app.use(
+    rateLimit({
+      windowMs: RATE_LIMIT_MS * 60 * 1000, // 15 minutes
+      max: RATE_LIMIT_MAX, // limit each IP to 100 requests per windowMs
+    }),
+  );
+
+  // validation pipe
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (errors) => new BadRequestException(errors),
+  }));
+
+  // swagger documentation
+  const options = new DocumentBuilder()
+    .setTitle('Boilerplate API')
+    .setDescription('Boilerplate API Documention')
+    .setVersion('1.0')
+    .addTag('Boilerplate API')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  // serve app
+  await app.listen(APP_PORT);
 }
 bootstrap();
